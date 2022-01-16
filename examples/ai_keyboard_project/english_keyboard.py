022cee9b4c3383a89ac9566d96892fbf02ef3a8e
Up & Daun <updauney@daum.net>
2022-01-17 02:54:54 +0900

    edit example english keyboard (add shift key and backspace key)

--- examples/ai_keyboard_project/english_keyboard.py
+++ examples/ai_keyboard_project/english_keyboard.py
@@ -5,14 +5,21 @@ import numpy as np
 import cvzone
 from pynput.keyboard import Controller
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 cap.set(3, 1280)
 cap.set(4, 720)
 
 detector = HandDetector(detectionCon=0.8)
-keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
-        ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
-        ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"]]
+
+keys = [["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "<"],
+        ["a", "s", "d", "f", "g", "h", "j", "k", "l", ";"],
+        ["z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "^"]]
+
+shift_keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "<"],
+              ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
+              ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "^"]]
+
+
 
 
 # def drawALL(img, buttonList):
@@ -50,12 +57,22 @@ class Button():
         
 
 buttonList = []
+shift_buttonList = []
+selected_buttonList = []
+finalText = ''
+typing_trigger = False
+shift_trigger = False
+keyboard = Controller()
+
+
 for i in range(len(keys)):
     for j, key in enumerate(keys[i]):
         buttonList.append(Button([100*j+50, 100*i+50], key))
 
-finalText = ''
-keyboard = Controller()
+for i in range(len(shift_keys)):
+    for j, key in enumerate(shift_keys[i]):
+        shift_buttonList.append(Button([100*j+50, 100*i+50], key))
+
 
 while True:
     success, img = cap.read()
@@ -63,10 +80,15 @@ while True:
     _, img = detector.findHands(img, flipType=False)
     lmList, bboxInfo = detector.findPosition(img)
 
-    img = drawALL(img, buttonList)
+    if shift_trigger:
+        img = drawALL(img, shift_buttonList)
+        selected_buttonList = shift_buttonList
+    else:
+        img = drawALL(img, buttonList)
+        selected_buttonList = buttonList
 
     if lmList:
-        for button in buttonList:
+        for button in selected_buttonList:
             x,y = button.pos
             w,h = button.size
 
@@ -74,19 +96,37 @@ while True:
                 cv2.rectangle(img, (x-5,y-5), (x+w+5, y+h+5), (175,0,175), cv2.FILLED)
                 cv2.putText(img, button.text, (x+20, y+65), cv2.FONT_HERSHEY_PLAIN,
                             4, (255,255,255), 4)
-                l, _ = detector.findDistance(8, 12)
-                print(l)
+                length, _ = detector.findDistance(8, 12)
+                # print(l)
 
                 # when clicked
-                if l < 30:
-                    # pynput
-                    keyboard.press(button.text)
-
+                if length < 35:
                     cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), cv2.FILLED)
                     cv2.putText(img, button.text, (x+20, y+65), cv2.FONT_HERSHEY_PLAIN,
                                 4, (255,255,255), 4)
-                    finalText += button.text
-                    time.sleep(0.1)                                
+                    if typing_trigger == False:
+                        if button.text == "<":
+                            if len(finalText) != 0:
+                                finalText = finalText[:-1]
+                                typing_trigger = True
+                        elif button.text == "^":
+                            if shift_trigger:
+                                shift_trigger = False
+                            else:
+                                shift_trigger = True
+                            typing_trigger = True
+                        else:
+                            # pynput
+                            keyboard.press(button.text)
+                            finalText += button.text
+                            typing_trigger = True
+                else:
+                    typing_trigger = False
+
+    if shift_trigger:
+        cv2.rectangle(img, (1050,250), (1135, 335), (0,255,0), cv2.FILLED)
+        cv2.putText(img, "^", (1070, 315), cv2.FONT_HERSHEY_PLAIN,
+                    4, (255,255,255), 4)      
 
     cv2.rectangle(img, (50, 350), (700, 450), (175,0,175), cv2.FILLED)
     cv2.putText(img, finalText, (60, 430), cv2.FONT_HERSHEY_PLAIN,
